cb78e8f95882c796708936d6426eb05318096a83
JasonLeor <305859613@qq.com>
2017-05-22 12:22:12 +0800

    dubbo + zeekeeper  分布式架构 demo
    Signed-off-by: jasonleor <305859613@qq.com>

--- service_consumer/src/main/java/Run.java
+++ service_consumer/src/main/java/Run.java
@@ -1,18 +1,33 @@
+import com.alibaba.dubbo.rpc.RpcContext;
+import com.alibaba.dubbo.rpc.service.EchoService;
 import com.jason.freamwork.service.ServiceApi;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 /**
  * Created by suliguo on 2017/5/22.
  */
 public class Run {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException, ExecutionException {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("config/spring_dubbo.xml");
         context.start();
         // 注意这个 bean 在改项目中并没有这个bean的实现
         //这里是 通过dubbo在zookeeper的服务列表中 通过 HelloService 获取服务
         ServiceApi api = (ServiceApi) context.getBean("HelloService");
-        System.out.println(api.sayHello("this dubbo zookeeper"));
+        // 1
+        String result =api.sayHello("1");
+//        for (int i =0;i<10;i++) {
+//            System.out.println(api.sayHello("0"));
+//        }
+        Future future = RpcContext.getContext().getFuture();
+
+        // 这样调用的作用是 让1处代码异步执行 然后在这句代码等待会调用
+        // 还可以 使用callback 异步回调方法 然后不用在改处 等待
+        //这种编程 像是 js 的callback机制
+        result = (String) future.get();
+        System.out.println(result);
     }
 }
--- service_provider/src/main/java/com/jason/freamwork/impl/ServiceImpl.java
+++ service_provider/src/main/java/com/jason/freamwork/impl/ServiceImpl.java
@@ -1,15 +1,31 @@
 package com.jason.freamwork.impl;
 
+import com.alibaba.dubbo.config.annotation.Service;
 import com.jason.freamwork.service.ServiceApi;
+import com.sun.org.apache.xpath.internal.SourceTree;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Created by suliguo on 2017/5/22.
  */
+@Component
+@Service(timeout = 5000,retries = 0)//retries 超时重连次数
 public class ServiceImpl implements ServiceApi{
+    private final AtomicInteger i = new AtomicInteger();
     public String sayHello(String word) {
-        return "hello "+word;
+        String result = "request:"+word+",response:"+i.getAndIncrement();
+        System.out.println(result);
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 
     public List getUsers() {
